6bb7c72455b7ca877399eff0cc2891b9b96baea6
digeoesp <diegocrypto@outlook.com>
2021-03-30 00:48:47 -0700

    added a about path

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [  # create a list to handle all thee paths
-    path('', views.index, name='index') # when we hit the home route it will run the http response of views.index
+urlpatterns = [  # create a list to handle all the paths
+    path('', views.index, name='index'), # when we hit the home route it will run the http response of views.index
+    path('about/', views.about, name='about') #specify the path
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,11 @@ from django.http import HttpResponse # need this to send a response to the user
 #
 def index(request):
     return HttpResponse('<h1>K Cal</h1>')
+
+def about(request):
+    lorem_ipsum = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+    return HttpResponse(lorem_ipsum)
+
+
+# 1)Make a view function
+# 2) add the view to the urls.py inside main_app.urls file
