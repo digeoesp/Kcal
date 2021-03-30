daf5fc830f88a75ff3aa8a709a164b8a23c56087
digeoesp <diegocrypto@outlook.com>
2021-03-30 01:15:00 -0700

    added index and about html

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [  # create a list to handle all the paths
     path('', views.index, name='index'), # when we hit the home route it will run the http response of views.index
-    path('about/', views.about, name='about') #specify the path
+    path('about/', views.about, name='about'), #specify the path
+    path('contact/', views.contact, name='contact')
 
 ]
