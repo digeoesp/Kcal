e65387fe58c53a263a4f9ba230f359bfd0867cc4
digeoesp <diegocrypto@outlook.com>
2021-03-30 20:20:50 -0700

    added field models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Food,Profile,PostFood
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    readonly_fields = ('date',)
+admin.site.register(Food)
+admin.site.register(Profile)
