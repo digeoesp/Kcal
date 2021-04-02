a07962a65ccdaa16a18bbaba8b1e431f3d11f528
mespinoza30 <m.espinoza16@hotmail.com>
2021-04-02 10:34:37 -0700

    API TEST

--- Kcal/settings.py
+++ Kcal/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -81,7 +81,6 @@ DATABASES = {
         'NAME': 'kcal111',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
--- main_app/urls.py
+++ main_app/urls.py
@@ -9,7 +9,5 @@ urlpatterns = [  # create a list to handle all the paths
     path('food/', views.food_index, name='food'),
     path('profile/', views.profile_index, name='profile' ),
     path('accounts/signup', views.sign_up, name='sign_up'),
-  
-
 
 ]
