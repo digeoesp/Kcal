0c2b263d1d4bfddfe67ccac569af385fc0e557a0
digeoesp <diegocrypto@outlook.com>
2021-04-11 01:44:09 -0700

    .gitignore

--- Kcal/settings.py
+++ Kcal/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'HELLO'
+SECRET_KEY = 'SECRET_KEY'
 
 GOOGLE_RECAPTCHA_SECRET_KEY = '6Ld86JwaAAAAAMmZqGFGC42RWOfYwmtYBU0Czrpm'
 
